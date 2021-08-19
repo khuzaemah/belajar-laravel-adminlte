fdb340186e5d23899b061c0bee255f0c1056dc4f
Emah Khujaemah <khuza.emah24@gmail.com>
2021-08-19 16:56:26 +0700

    tutorial selesai 18 video dari Padang Tekno

--- resources/views/layout/v_nav.blade.php
+++ resources/views/layout/v_nav.blade.php
@@ -1,27 +1,27 @@
 
 <ul class="sidebar-menu" data-widget="tree">
-  <li class="header">MAIN NAVIGATION</li>
-  <li class="{{ request()->is('/') ? 'active' : ''}}" ><a href="/"><i class="fa fa-dashboard"></i> <span>Home</span></a></li>
-  @if (auth()->user()->level==1)          
-  <li class="{{ request()->is('dosen') ? 'active' : ''}}"><a href="/dosen"><i class="fa fa-book"></i> <span>Dosen</span></a></li>
-  <li class="{{ request()->is('mahasiswa') ? 'active' : ''}}"><a href="/mahasiswa"><i class="fa fa-book"></i> <span>Mahasiswa</span></a></li>
-  @endif
+    <li class="header">MAIN NAVIGATION</li>
+      <li class="{{ request()->is('/') ? 'active' : ''}}" ><a href="/"><i class="fa fa-dashboard"></i> <span>Home</span></a></li>
+      
+      @if (auth()->user()->level==1)          
+      <li class="{{ request()->is('dosen') ? 'active' : ''}}"><a href="/dosen"><i class="fa fa-book"></i> <span>Dosen</span></a></li>
+      <li class="{{ request()->is('mahasiswa') ? 'active' : ''}}"><a href="/mahasiswa"><i class="fa fa-book"></i> <span>Mahasiswa</span></a></li>
 
-  @if (auth()->user()->level==2)
-    <li class="{{ request()->is('user') ? 'active' : ''}}"><a href="/user"><i class="fa fa-book"></i> <span>User</span></a></li>
-  @endif
+      @elseif (auth()->user()->level==2)
+        <li class="{{ request()->is('user') ? 'active' : ''}}"><a href="/user"><i class="fa fa-book"></i> <span>User</span></a></li>
 
-  <li class="treeview">
-    <a href="#">
-      <i class="fa fa-share"></i> <span>Guest</span>
-      <span class="pull-right-container">
-        <i class="fa fa-angle-left pull-right"></i>
-      </span>
-    </a>
-    <ul class="treeview-menu">
-      <li class="{{ request()->is('guest') ? 'active' : ''}}"><a href="/guest"><i class="fa fa-circle-o"></i> <span>Pelanggan 1</span></a></li>
-      <li class="{{ request()->is('guest') ? 'active' : ''}} "><a href="/guest"><i class="fa fa-circle-o"></i> <span>Pelanggan 2</span></a></li>
-    </ul>
-  </li>
-  
+      @elseif (auth()->user()->level==3)
+        <li class="treeview">
+        <a href="#">
+          <i class="fa fa-share"></i> <span>Guest</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li class="{{ request()->is('guest') ? 'active' : ''}}"><a href="/guest"><i class="fa fa-circle-o"></i> <span>Pelanggan 1</span></a></li>
+          <li class="{{ request()->is('guest') ? 'active' : ''}} "><a href="/guest"><i class="fa fa-circle-o"></i> <span>Pelanggan 2</span></a></li>
+        </ul>
+    </li> 
 </ul>
+@endif
